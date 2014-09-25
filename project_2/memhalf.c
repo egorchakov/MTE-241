2728c0c0b72df5cfccdb22b1a7f2715715065267
Ev <evgorchakov@gmail.com>
2014-09-25 16:43:14 -0400

    Fixing bucket index calculation

--- project_2/memhalf.c
+++ project_2/memhalf.c
@@ -55,6 +55,8 @@ void* half_alloc(size_t n){
 	int i = 0;
 	if(m > MAX_MEMORY) return NULL;
 	i = get_alloc_bucket_index(m);
+	while (i<NUM_BUCKETS && mprgmmap[i] == NULL) i++;
+	if (i >= NUM_BUCKETS || mprgmmap[i] == NULL) return NULL;
 	if(i >= NUM_BUCKETS || mprgmmap[i] == NULL) return NULL; // Out of memory
 	memmap_free_t* mmap = mprgmmap[i];
 	memmap_t* mmap_alloc = (memmap_t*)mmap;
