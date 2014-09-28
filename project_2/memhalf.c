3d36246794fa736f775d16879459ccd8cae99e5d
jtmzheng <jtmzheng@gmail.com>
2014-09-28 19:14:11 -0400

    First working implementation of insert
    - Fixed packing (4 bytes now, yay!)
    - Moved util functions to memhalf (temporary until build system is
    fixed)
    - Fixed insert (32 byte request tested)
    - Removed call to half_free in half_insert (half_free assumes existance
    of header before the void*)

--- project_2/memhalf.c
+++ project_2/memhalf.c
@@ -24,6 +24,29 @@ void* get_next_free(memmap_free_t const* mmap){
 	return (void*)mmap->next_free;
 }
 
+// Temporarily move from util.c
+S16 floor_log2(size_t size){
+    S16 i = 0;
+    while (size >>= 1) i++;
+    return i;
+}
+
+S16 ceil_log2(size_t size){
+    return ( size == 1 ) ? 0 : floor_log2(size - 1) + 1;
+}
+
+S16 get_alloc_bucket_index(size_t size){
+    // 2^6 = 64 is the first bucket, therefore indices are offset by 6
+    S16 index = ceil_log2(size) - 6;
+    return (index >=0 ) ? index : 0; 
+}
+
+S16 get_free_bucket_index(size_t size){
+    // 2^6 = 64 is the first bucket, therefore indices are offset by 6
+    S16 index = floor_log2(size) - 6;
+    return (index >=0) ? index : 0;
+}
+
 void memmap_init(memmap_t* const mmap, size_t size){
 	// Initialize the values for the fields
 	mmap->prev_block = 0;
@@ -47,6 +70,7 @@ void half_init(){
 	mprgmmap[NUM_BUCKETS - 1] = block;
 	#ifdef DEBUG_MEMORY
 	free_memory = MAX_MEMORY;
+	printf("\nHalf init succeeded: %d bytes free\n", free_memory);
 	#endif
 }
 
@@ -55,14 +79,27 @@ void* half_alloc(size_t n){
 	int i = 0;
 	if(m > MAX_MEMORY) return NULL;
 	i = get_alloc_bucket_index(m);
-	while (i<NUM_BUCKETS && mprgmmap[i] == NULL) i++;
+	while (i < NUM_BUCKETS && mprgmmap[i] == NULL) i++;
+
+	#ifdef DEBUG_MEMORY
+	printf("Requested m bytes: %d\n", m);
+	printf("Getting memory from bucket: %d\n", i);
+	#endif
+
 	if(i >= NUM_BUCKETS || mprgmmap[i] == NULL) return NULL; // Out of memory
 	memmap_free_t* mmap = mprgmmap[i];
 	memmap_t* mmap_alloc = (memmap_t*)mmap;
 	void* ptr_next = get_next_free(mmap);
 
+	#ifdef DEBUG_MEMORY
+	printf("Allocated block memory: %p\n", mmap);
+	#endif
+
 	// Remove allocated block from LL
 	if(ptr_next){
+		#ifdef DEBUG_MEMORY
+		printf("Removed block from LL: %p\n", ptr_next);
+		#endif
 		memmap_free_t* mmap_next_free = (memmap_free_t*)(ptr_next - sizeof(memmap_free_t));
 		mmap_next_free->prev_free = NULL;
 		mprgmmap[i] = mmap_next_free; 
@@ -71,8 +108,11 @@ void* half_alloc(size_t n){
 	size_t mmap_size = get_block_size(mmap_alloc);
 	mmap_alloc->alloc = __TRUE;
 	if(mmap_size - m > 32) {
-		void* new_block = ((void*)mmap) + sizeof(memmap_free_t) + m;
-		half_free(new_block);
+		void* new_block = ((void*)mmap) + m;
+		// Can't call half_free cause half_free assumes header exists
+		#ifdef DEBUG_MEMORY
+		printf("Allocated memory size: %d\n", m);
+		#endif
 	}
 
 	#ifdef DEBUG_MEMORY
@@ -90,7 +130,7 @@ void half_free(void* ptr){
 	((memmap_t*) block->memmap)->alloc = false;
 	
 	// which bucket to insert into?
-	S16 bucket_index = get_free_bucket_index(((memmap_t*) block->memmap)->block_size);
+	S16 bucket_index = get_free_bucket_index((size_t)((memmap_t*) block->memmap)->block_size);
 
 	// prepend to the corresponding linked list
 	memmap_free_t* top_block = mprgmmap[bucket_index];
@@ -103,10 +143,12 @@ void half_free(void* ptr){
 int main( int argc, char *argv[] ){
 	printf("520 -> %d\n", CEIL32(520));
 	printf("32 -> %d\n", CEIL32(32));
-	printf("%d\n", get_alloc_bucket_index(1000));
-	printf("%d\n", get_free_bucket_index(1000));
+	printf("%d\n", get_alloc_bucket_index((size_t)1000));
+	printf("%d\n", get_free_bucket_index((size_t)1000));
 
 	half_init();
 	void* dummy = half_alloc(32);
 	printf("\nFree memory: %d\n", free_memory);
+	fgetc(stdin);
+	return 0;
 }
--- project_2/util.h
+++ project_2/util.h
@@ -9,10 +9,12 @@
 
 #define CEIL32(n) (((n-1) >> 5) +1) << 5
 
+#ifdef USE_UTIL
 S16 floor_log2(size_t );
 S16 ceil_log2(size_t );
 
 S16 get_alloc_bucket_index(size_t );
 S16 get_free_bucket_index(size_t );
+#endif
 
 #endif
