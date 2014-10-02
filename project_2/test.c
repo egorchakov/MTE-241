6debc46c2fc4530f7fb04a74cc3edbd1e325740d
Ev <evgorchakov@gmail.com>
2014-10-02 19:10:15 -0400

    Better output

--- project_2/test.c
+++ project_2/test.c
@@ -5,13 +5,23 @@
 #include "type.h"
 #include "memhalf.h"
 
-#define RED     "\x1b[31m"
-#define GREEN   "\x1b[32m"
-#define YELLOW  "\x1b[33m"
-#define BLUE    "\x1b[34m"
-#define MAGENTA "\x1b[35m"
-#define CYAN    "\x1b[36m"
-#define RESET   "\x1b[0m"
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
 
 #define PRINT_IGNORE_EMPTY_BUCKETS
 
@@ -20,7 +30,7 @@ void print_bucket(memmap_free_t* head){
     memmap_t* tmp_alloc = NULL;
     while (tmp_free){
         tmp_alloc = (memmap_t*) tmp_free;
-        printf("[%d] --> ", get_block_size(tmp_alloc));
+        printf("|%d|-> ", get_block_size(tmp_alloc));
         if (is_last_in_bucket(tmp_free)) break;
         tmp_free = get_next_free(tmp_free);
     }
@@ -37,19 +47,19 @@ void print_buckets(memmap_free_t* buckets[], short size){
         printf("%d (%d -- %d ): ", i, 1 << i+5, (1 << i+6) -1);
         print_bucket(buckets[i]);
     }
-    printf("\n===================== END BUCKETS LAYOUT =====================\n" RESET);
+    printf("\n===================== END BUCKETS LAYOUT =====================\n\n" RESET);
 }
 
 void print_memory_layout(void* base_ptr){
     memmap_t* block = (memmap_t*) base_ptr;
-    printf( GREEN "==================== START MEMORY LAYOUT ====================\n\n");
+    printf( GREEN "==================== START MEMORY LAYOUT ====================\n\n" RESET);
     while (block){
-        if (get_allocated(block)) printf("( %d )->", get_block_size(block));
-        else printf("[ %d ]->", get_block_size(block));
+        if (get_allocated(block)) printf(BOLDGREEN "[%d]->" RESET, get_block_size(block));
+        else printf(GREEN "|%d|->" RESET, get_block_size(block));
         if (is_last_in_memory(block)) break;
         block = get_next_block(block);
     }
-    printf("\n\n==================== END MEMORY LAYOUT ====================\n" RESET);
+    printf( GREEN "\n\n==================== END MEMORY LAYOUT ====================\n\n" RESET);
 }
     
 void main(void){
