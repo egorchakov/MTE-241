6f1b7b25fcc6e4733a64d6b6b0385c631de3d301
Evgeny <evgorchakov@gmail.com>
2014-10-17 13:54:48 -0400

    Inlining + defining functions in the header file

--- project_2/util.h
+++ project_2/util.h
@@ -12,10 +12,24 @@
 #define CEIL32(n) (((n-1) >> 5) +1) << 5
 #define FLOOR32(n) n >> 5 << 5
 
-S16 floor_log2(U32 );
-S16 ceil_log2(U32 );
+__inline S16 floor_log2(U32 size){
+    S16 i = 0;
+    while (size >>= 1) i++;
+    return i;
+}
 
-S16 get_alloc_bucket_index(U32 );
-S16 get_free_bucket_index(U32 );
+__inline S16 ceil_log2(U32 size){
+    return ( size == 1 ) ? 0 : floor_log2(size - 1) + 1;
+}
+
+__inline S16 get_alloc_bucket_index(U32 size){
+    S16 index = ceil_log2(size) - FIRST_BUCKET_POWER;
+    return (index >=0 ) ? index : 0; 
+}
+
+__inline S16 get_free_bucket_index(U32 size){
+    S16 index = floor_log2(size) - FIRST_BUCKET_POWER;
+    return (index >=0) ? index : 0;
+}
 
 #endif
