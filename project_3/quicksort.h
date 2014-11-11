cabc122fb03e898d7885a14e90fc69eaa93699db
Evgeny <evgorchakov@gmail.com>
2014-11-10 21:05:59 -0500

    Temporarily moved structs to quicksort.h + quicksort_sem

--- project_3/quicksort.h
+++ project_3/quicksort.h
@@ -3,8 +3,18 @@
 
 #include "array_tools.h"
 
+typedef struct {
+    array_t array;
+    size_t a;
+    size_t c;
+} array_interval_t;
+
+typedef struct{
+    array_interval_t interval;
+} qsort_task_parameters_t;
+
 void quicksort( array_t );
 // Uncomment this when you implement Project 4
-// void quicksort_sem( array_t );
+void quicksort_sem( array_t );
 
 #endif
