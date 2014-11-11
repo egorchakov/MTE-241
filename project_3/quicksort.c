509ee2d17261a22bd349996dc3d48a0c5dcdf977
Evgeny <evgorchakov@gmail.com>
2014-11-10 21:05:43 -0500

    WIP

--- project_3/quicksort.c
+++ project_3/quicksort.c
@@ -92,71 +92,88 @@ int partition(array_interval_t* interval){
 }
 
 __task void quick_sort_task( void* void_ptr){
-  // Your implementation here
-// void quick_sort_task(void* void_ptr){
-    
-    OS_MUT left_task_done, right_task_done;
+
+    OS_TID left, right;
     int pivot_index;
-    
     array_interval_t* interval;
-    array_interval_t left_interval;
-    array_interval_t right_interval;
+    array_interval_t* left_interval;
+    array_interval_t* right_interval;
     qsort_task_parameters_t* cur_params;
-    qsort_task_parameters_t left_task_params;
-    qsort_task_parameters_t right_task_params;
+    qsort_task_parameters_t* left_task_params;
+    qsort_task_parameters_t* right_task_params;
 
     cur_params = (qsort_task_parameters_t*) void_ptr;
     interval = &(cur_params->interval);
-    
-    if (interval->array.length > 1){
-        // if (interval->array.length <= USE_INSERTION_SORT){
-        //     insertion_sort(interval);
-        //     return;
-        // }
+
+    left_interval = (array_interval_t*) malloc(sizeof(array_interval_t));
+    right_interval = (array_interval_t*) malloc(sizeof(array_interval_t));
+    left_task_params = (qsort_task_parameters_t*) malloc(sizeof(qsort_task_parameters_t));
+    right_task_params = (qsort_task_parameters_t*) malloc(sizeof(qsort_task_parameters_t));
+
+    if (left_interval == NULL || right_interval == NULL || left_task_params == NULL || right_task_params == NULL)
+        MUTEXPRINT("[[[ MALLOC FAILED ]]]\n");
+   
+    if (interval->array.length > 1) { 
 
         pivot_index = partition(interval);
+        left_interval->array = interval->array;
+        left_interval->a = interval->a;
+        left_interval->c = pivot_index - 1;
+        left_interval->array.length = left_interval->c - left_interval->a + 1;
 
-        // array_interval_init(&left_interval, &(interval->array), interval->a, interval->c);
-        // array_interval_init(&right_interval, &(interval->array), interval->a, interval->c);
-        left_interval.array = interval->array;
-        left_interval.a = interval->a;
-        left_interval.c = pivot_index - 1;
-        left_interval.array.length = left_interval.c - left_interval.a + 1;
+        right_interval->array = interval->array;
+        right_interval->a = pivot_index + 1;
+        right_interval->c = interval->c;
+        right_interval->array.length = right_interval->c - right_interval->a + 1;
 
-        right_interval.array = interval->array;
-        right_interval.a = pivot_index + 1;
-        right_interval.c = interval->c;
-        right_interval.array.length = right_interval.c - right_interval.a + 1;
+        left_task_params->interval = *left_interval;
+      
+        right_task_params->interval = *right_interval;
 
-        left_task_params.interval = left_interval;
-        left_task_params.priority = cur_params->priority + 1;
+        os_mut_wait(&num_tasks_mutex, 0xffff);{
+            left = os_tsk_create_ex(quick_sort_task, 1, left_task_params);
+            num_tasks ++;
+            // MUTEXPRINT("PASSD TO L[%d]: a = %d, c = %d, length = %d \n", left, left_interval->a, left_interval->c, left_interval->array.length);
+        } os_mut_release(&num_tasks_mutex); 
 
-        right_task_params.interval = right_interval;
-        right_task_params.priority = cur_params->priority + 1;
-
-        os_tsk_create_ex(quick_sort_task, left_task_params.priority, &left_task_params);
-        os_tsk_create_ex(quick_sort_task, right_task_params.priority, &right_task_params);
+        os_mut_wait(&num_tasks_mutex, 0xffff);{
+            right = os_tsk_create_ex(quick_sort_task, 1, right_task_params);
+            num_tasks ++;
+            // MUTEXPRINT("PASSD TO L[%d]: a = %d, c = %d, length = %d \n", left, left_interval->a, left_interval->c, left_interval->array.length);
+        }
     }
 
+    os_mut_wait(&num_tasks_mutex, 0xffff);{
+        num_tasks--;
+        MUTEXPRINT("%d tasks\n", num_tasks);
+        if (num_tasks == 0){
+            os_sem_send(&all_tasks_finished);
+        }
+    } os_mut_release(&num_tasks_mutex);
+
     os_tsk_delete_self();
-    printf("done\n");
 }
 
 void quicksort_sem( array_t array ) {
+
 	array_interval_t interval;
 	qsort_task_parameters_t task_param;
-	
-	// Based on MTE 241 course notes--you can change this if you want
-	//  - in the course notes, this sorts from a to c - 1
-	interval.array =  array;
-	interval.a     =  0;
-	interval.c     =  array.length-1;
-	
-	task_param.interval = interval;
+    // Based on MTE 241 course notes--you can change this if you want
+    //  - in the course notes, this sorts from a to c - 1
+    interval.array =  array;
+    interval.a     =  0;
+    interval.c     =  array.length-1;
+    task_param.interval = interval;
 
-	// If you are using priorities, you can change this
-	task_param.priority = 10;
-	
-	//start the quick_sort threading
-	os_tsk_create_ex( quick_sort_task, task_param.priority, &task_param ); 
+    os_mut_init(&printing);
+
+    os_mut_init(&num_tasks_mutex);
+    os_sem_init(&all_tasks_finished, 0);
+
+    os_mut_wait(&num_tasks_mutex, 0xffff);{
+       os_tsk_create_ex( quick_sort_task, 1, &task_param );
+       num_tasks = 1;
+    } os_mut_release(&num_tasks_mutex);
+
+    os_sem_wait(&all_tasks_finished, 0xffff);
 }
