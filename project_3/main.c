d951a1c13e8e59be035e9d86130e8875f150d9a7
Evgeny <evgorchakov@gmail.com>
2014-11-11 20:55:28 -0500

    Fixed main (revert to Harder's)

--- project_3/main.c
+++ project_3/main.c
@@ -1,4 +1,3 @@
-
 #include <LPC17xx.h>
 #include <RTL.h>
 #include <stdio.h>
@@ -6,7 +5,6 @@
 #include "quicksort.h"
 #include "array_tools.h"
 
-#define PRINT_ARRAY
 #undef PRINT_ARRAY
 
 // Change 'undef' to 'define' when you have implemented quicksort_sem
@@ -18,74 +16,59 @@
 **/
 
 volatile double time = 0;
+
 /**
  *    'bask_task' is the main task that never stops. It caoomunicates with
  *  the user and creates new thread(s) to sort, check, and print the sorted
  *  array.
  **/
 
-
-// __task void task2(void){
-// 	os_sem_wait(&mutex, 0xffff);
-// 		// printf("2: semaphore not attainable");
-// 	printf("task2 success\n");
-// }
-
-// __task void task1(void){
-// 	os_sem_wait(&mutex, 0xffff);
-// 	printf("task1 success\n");
-// }
-
-
 __task void base_task( void ) {
-	array_t array;
-	// Set the priority of the base task
-	//  - lowest priority:     1
-	//  - highest priority:  254
+    array_t array;
+    // Set the priority of the base task
+    //  - lowest priority:     1
+    //  - highest priority:  254
 
-	os_tsk_prio_self( 1 );
-	
-	while ( 1 ) {
-		array = generate_array();
-		printf("Array starts at %p\n", array.array);
+    os_tsk_prio_self( 250 );
 
-		time = os_time_get();
-		// Sort array
-		// print_array(&array);
-		// #ifdef TEST_SEMAPHORE_IMPLEMENTATION
-			quicksort_sem( array );
-		// #else
-		// 	quicksort( array );
-		// #endif
+    while ( 1 ) {
+        array = generate_array();
 
-		time = os_time_get() - time;
+        time = os_time_get();
 
-		if( is_sorted_array( &array ) )
-			printf("The array is sucessfully sorted in %.1f us.\n", time);
-		else {
-			printf("The array is not sorted!\n");
-			print_array(&array);
-		}
+        // Sort array
+        #ifdef TEST_SEMAPHORE_IMPLEMENTATION
+            quicksort_sem( array );
+        #else
+            quicksort( array );
+        #endif
 
+        time = os_time_get() - time;
 
-		#ifdef PRINT_ARRAY
-			print_array( &array );
-		#endif
+        if( is_sorted_array( &array ) )
+            printf("The array is sucessfully sorted in %.1f us.\n", time);
+        //else
+        //  printf("The array is not sorted!\n");
 
-		free(array.array);
-	}
+        #ifdef PRINT_ARRAY
+            print_array( &array );
+        #endif
+    }
 }
 
 int main( void ) {
-	SystemInit();
-	SystemCoreClockUpdate();
 
-	printf(" ");
+    SystemInit();
+    SystemCoreClockUpdate();
 
-	// We will change thvalue of the seed for the teste 
-	srand( 10 );
-	os_sys_init( base_task );
-	while ( 1 ) {
-		// Endless loop
-	}
+    printf(" ");
+
+    // We will change the value of the seed for the test
+    srand( 10 );
+
+    os_sys_init( base_task );
+
+    while ( 1 ) {
+        // Endless loop
+    }
 }
