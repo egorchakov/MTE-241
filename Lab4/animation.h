a9f3c5824cc94dacd5a0eea9b1ef281b222d5d41
Evgeny <evgorchakov@gmail.com>
2014-11-18 22:21:55 -0500

    Just a header for animation.c

--- Lab4/animation.h
+++ Lab4/animation.h
@@ -1,5 +1,16 @@
 #ifndef _ANIMATION_H
 #define _ANIMATION_H
 
-#include "tasks.h"
+#include <LPC17xx.h>
+#include <RTL.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "config.h"
+#include "circle.h"
+
+typedef circle_t object_t;
+
+extern volatile int active_objects;
+extern object_t objects[MAX_OBJECTS];
+
 #endif
