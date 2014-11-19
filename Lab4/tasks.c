1d183d39b87f1258aca2cee53b2a5528979eef01
Evgeny <evgorchakov@gmail.com>
2014-11-18 22:24:29 -0500

    WIP on tasks

--- Lab4/tasks.c
+++ Lab4/tasks.c
@@ -1,13 +1,79 @@
 #include <rtl.h>
 #include "tasks.h"
 
-#define HIGHEST 254
-#define LOWEST 1
-#define INIT_COLOR Green
+OS_SEM add_random_object_sem;
+OS_SEM redraw_objects_sem;
+OS_SEM redraw_objects_done_sem;
+
+__task void add_random_object(void){
+    int i,j;
+    object_t obj;
+        
+        while(1){
+        os_sem_wait(&add_random_object_sem, FOREVER);
+        // will possibly need a mutex for active_objects
+        if (active_objects < MAX_OBJECTS){
+            srand(os_time_get() % 100);
+            obj.x = rand() % 300;
+            obj.y = rand() % 300;
+            obj.radius = 5 + rand() % 10;
+            objects[active_objects++] = obj;
+        }
+    }
+}
+
+__task void main_task(void){
+    int i,x;
+    while(1){
+        for (i=0; i<active_objects; i++){
+            // just moving along the x axis
+            x = objects[i].x;
+            objects[i].x = (x < 320 ? x + 20 : 20);
+        }
+        os_sem_send(&redraw_objects_sem);
+        os_sem_wait(&redraw_objects_done_sem, FOREVER);
+    }
+}
+
+__task void redraw_objects(void){
+    int i;
+    object_t obj; 
+
+
+    os_sem_wait(&redraw_objects_sem, FOREVER);
+    
+    GLCD_Clear(INIT_COLOR);
+    for (i = 0; i< active_objects; i++){
+        obj = objects[i];
+        // assuming objects are rectangles for now
+        draw_rectangle(obj.x, obj.y, obj.radius*2, obj.radius*2, Red);
+    }
+
+    os_sem_send(&redraw_objects_done_sem);
+}
 
 __task void init_task(void){
-    os_tsk_prio_self(HIGHEST);
+    object_t obj;
+    os_tsk_prio_self(HIGHEST_PRIO);
+
+    // create a dummy object
+    obj.x = 20;
+    obj.y = 20;
+    obj.radius = 10;
+    objects[0] = obj;
+    active_objects++;   
+
     GLCD_Init();
     GLCD_Clear(INIT_COLOR);
-    os_tsk_prio_self(LOWEST);
+        
+    os_tsk_create(main_task, LOWEST_PRIO+1);
+    os_tsk_create(add_random_object, LOWEST_PRIO+2);
+    os_tsk_create(redraw_objects, LOWEST_PRIO+1);
+    
+    os_sem_init(&add_random_object_sem, 0);
+    os_sem_init(&redraw_objects_sem, 0);
+    os_sem_init(&redraw_objects_done_sem, 0);
+    
+    os_tsk_prio_self(LOWEST_PRIO);
+    while(1);
 }
--- Lab4/tasks.h
+++ Lab4/tasks.h
@@ -4,8 +4,19 @@
 #include <LPC17xx.h>
 #include <RTL.h>
 #include "GLCD.h"
+#include "config.h"
+#include "circle.h"
+#include "animation.h"
+#include "util.h"
+
+extern OS_SEM add_random_object_sem;
+extern OS_SEM redraw_objects_sem;
+extern OS_SEM redraw_objects_done_sem;
+extern OS_SEM done;
 
 __task void init_task(void);
-__task void redraw_objects(void* args);
+__task void redraw_objects(void);
+__task void add_object(void* args);
+__task void add_random_object(void);
 
 #endif
