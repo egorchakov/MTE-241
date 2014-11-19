d00203c79bae0214efa52176f496600537475a74
Evgeny <evgorchakov@gmail.com>
2014-11-18 22:26:24 -0500

    Global config file

---
+++ Lab4/config.h
@@ -0,0 +1,15 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+#define MAX_OBJECTS 10
+#define MAX_BITMAP_SIZE 400 // 20 x 20
+#define INIT_COLOR Green
+
+#define HIGHEST_PRIO 253
+#define LOWEST_PRIO 1
+
+#define ACTIVE 0x0001
+#define FOREVER 0xffff
+
+#endif
+
