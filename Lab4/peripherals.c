2aeec70fee6f5f70d61d1642b86b42e32f32b848
Evgeny <evgorchakov@gmail.com>
2014-11-18 22:23:11 -0500

    Adding IRQ Handler for the push button

--- Lab4/peripherals.c
+++ Lab4/peripherals.c
@@ -1,7 +1,8 @@
 #include "peripherals.h"
+#include "tasks.h"
 
 void INT0_init( void ) {
-    // src: io_example project by the TA
+    // [SRC]: io_example project by the TA
     LPC_SC->PCONP |= (1 << 12);
     // P2.10 is related to the INT0 or the push button.
     // P2.10 is selected for the GPIO 
@@ -21,5 +22,14 @@ void INT0_init( void ) {
 }
 
 void EINT3_IRQHandler(void){
-    ;
+    // [SRC]: io_example project by the TA
+    // Check whether the interrupt is called on the falling edge. GPIO Interrupt Status for Falling edge.
+    if ( LPC_GPIOINT->IO2IntStatF && (0x01 << 10) ) {
+        LPC_GPIOINT->IO2IntClr |= (1 << 10); // clear interrupt condition
+        // this should work, but doesn't
+        os_sem_send(&add_random_object_sem);
+        
+        // this part never gets called, and I don't know why
+        GLCD_Clear(Red);    
+    }
 }
