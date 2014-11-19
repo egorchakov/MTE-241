0d5c9caf8f4c2ffc04823b101e4a98fbbc9b3d66
Evgeny <evgorchakov@gmail.com>
2014-11-18 22:25:52 -0500

    Indentation

--- Lab4/util.c
+++ Lab4/util.c
@@ -1,25 +1,25 @@
 #include "util.h"
 
 void fill_rectangle(unsigned short* pBitmap, unsigned int w, unsigned int h, void* args) {
-	unsigned short* val = (unsigned short*)args;
-	unsigned short* pCurrent = pBitmap;
+    unsigned short* val = (unsigned short*)args;
+    unsigned short* pCurrent = pBitmap;
 
-	int i, j;
-	for(i = 0; i < w; i++) {
-		for(j = 0; j < h; j++) {
-			pCurrent[i * w + j] = *val; 
-		}
-	}		
+    int i, j;
+    for(i = 0; i < w; i++) {
+        for(j = 0; j < h; j++) {
+            pCurrent[i * w + j] = *val; 
+        }
+    }       
 }
 
 unsigned short* gen_bitmap(size_t w, size_t h, void(*fill_fn)(unsigned short*, unsigned int, unsigned int, void*), void* args) {
-	unsigned short* pBitmap = (unsigned short*)malloc(sizeof(unsigned short) * w * h);
-	fill_fn(pBitmap, w, h, args);
-	return pBitmap;
+    unsigned short* pBitmap = (unsigned short*)malloc(sizeof(unsigned short) * w * h);
+    fill_fn(pBitmap, w, h, args);
+    return pBitmap;
 }
 
 void draw_circle(circle_t c) {
-	// STUB
+    // STUB
 }
 
 void draw_rectangle(unsigned int x, unsigned int y, unsigned int w, unsigned int h, unsigned short val) {
